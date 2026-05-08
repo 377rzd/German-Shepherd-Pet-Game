ef87efec9494d0a17fe858f14b833d5fcbb3a635
377rzd <162637205+377rzd@users.noreply.github.com>
2026-05-08 08:53:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,41 @@ if (localStorage.getItem("firstPlay") == false) {
   var isFemale = localStorage.getItem("isFemale");
 } else {
   var hunger = 10;
-  var dirty = false;
-  var bored = false;
-  var tired = false;
+  var dirty = 30;
+  var bored = 5;
+  var tired = 60;
   var isFemale = false;
   localStorage.setItem("hunger", 10);
-  localStorage.setItem("dirty", false);
-  localStorage.setItem("bored", false);
-  localStorage.setItem("tired", false);
+  localStorage.setItem("dirty", 30);
+  localStorage.setItem("bored", 5);
+  localStorage.setItem("tired", 60);
   localStorage.setItem("isFemale", false);
   localStorage.setItem("firstPlay", false);
 }
-/* main loop statement
-while (true) {
-  localStorage.setItem("lastPlayed", Temporal.Now.plainDateTimeISO());
+// main loop statement
+setInterval(game(), 10000);
+
+function game() {
+  var random = math.random();
+  console.log(random);
+  if (random > 0.75 && hunger !< 0) {
+    hunger--;
+    localStorage.setItem("hunger", hunger);
+    console.log("Hunger decreased");
+  } else if (random > 0.5 && dirty !< 0) {
+    dirty--;
+    localStorage.setItem("dirty", dirty);
+    console.log("Dirty decreased");
+  } else if (random > 0.25 && bored !< 0) {
+    bored--;
+    localStorage.setItem("bored", bored);
+    console.log("Bored decreased");
+  } else if (tired !< 0) {
+    tired--;
+    localStorage.setItem("tired", tired);
+    console.log("Tired decreased");
+  } else {
+    console.log("All stats too low to decrease!");
+  }
 }
-*/
+
