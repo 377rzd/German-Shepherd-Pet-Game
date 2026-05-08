4daf1c05ef6dff20920e0d3d60cce0bf80ead7fa
377rzd <162637205+377rzd@users.noreply.github.com>
2026-05-07 20:28:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 if (localStorage.getItem("firstPlay") == false) {
-  let hunger = localStorage.getItem("hunger");
-  let dirty = localStorage.getItem("dirty");
-  let bored = localStorage.getItem("bored");
-  let tired = localStorage.getItem("tired");
-  let isFemale = localStorage.getItem("isFemale");
+  var hunger = localStorage.getItem("hunger");
+  var dirty = localStorage.getItem("dirty");
+  var bored = localStorage.getItem("bored");
+  var tired = localStorage.getItem("tired");
+  var isFemale = localStorage.getItem("isFemale");
 } else {
-  let hunger = 10;
-  let dirty = false;
-  let bored = false;
-  let tired = false;
-  let isFemale = false;
+  var hunger = 10;
+  var dirty = false;
+  var bored = false;
+  var tired = false;
+  var isFemale = false;
   localStorage.setItem("hunger", 10);
   localStorage.setItem("dirty", false);
   localStorage.setItem("bored", false);
