44b9298c9d7f496a16721d3eef2753a201db9b24
377rzd <162637205+377rzd@users.noreply.github.com>
2026-05-08 10:48:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// checkState checks dog states and changes image accordingly
+function checkState() {
+  var dogState = "https://377rzd.github.io/German-Shepherd-Pet-Game/assets/dog-"
+  if (hunger = 0) {
+    var dogState += "h-";
+  } else {
+    var dogState += "f-";
+  }
+  if (dirty = 0) {
+    var dogState += "d-";
+  } else {
+    var dogState += "c-";
+  }
+  if (tired = 0) {
+    var dogState += "t.png";
+  } else {
+    var dogState += "a.png";
+  }
+  document.getElementById("dog").src = dogState;
+}
 // Checks if the game has been opened before, setting and saving game variables
 if (localStorage.getItem("firstPlay") == false) {
   var hunger = localStorage.getItem("hunger");
@@ -5,6 +25,7 @@ if (localStorage.getItem("firstPlay") == false) {
   var bored = localStorage.getItem("bored");
   var tired = localStorage.getItem("tired");
   var isFemale = localStorage.getItem("isFemale");
+  checkState();
 } else {
   var hunger = 10;
   var dirty = 30;
@@ -26,18 +47,22 @@ function game() {
   console.log(random);
   if (random > 0.75 && hunger !< 0) {
     hunger--;
+    checkState();
     localStorage.setItem("hunger", hunger);
     console.log("Hunger decreased");
   } else if (random > 0.5 && dirty !< 0) {
     dirty--;
+    checkState();
     localStorage.setItem("dirty", dirty);
     console.log("Dirty decreased");
   } else if (random > 0.25 && bored !< 0) {
     bored--;
+    checkState();
     localStorage.setItem("bored", bored);
     console.log("Bored decreased");
   } else if (tired !< 0) {
     tired--;
+    checkState();
     localStorage.setItem("tired", tired);
     console.log("Tired decreased");
   } else {
